1e521b9ca97267e846c0f7a4d464d3327f361136
Максим <maxim.zhemerenko@outlook.om>
2015-03-10 20:32:57 +0200

    First class added

---
+++ Android/Sources/SupportLibrary/support.android/src/main/java/com/syject/support/android/FirstClass.java
@@ -0,0 +1,11 @@
+package com.syject.support.android;
+
+/**
+ * Created by Максим on 10.03.2015.
+ */
+public class FirstClass {
+    @Override
+    public String toString() {
+        return "Hello world!";
+    }
+}
